3cbc3a23ce4029475c1a2276d9a9ccd22a99b692
yasir shahid <yasirshahid5@gmail.com>
2020-11-17 00:43:33 +0500

    added Files

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,53 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import styled from 'styled-components'
+import { FaBars } from 'react-icons/fa'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+
+const Header = () => {
+  return (
+    <Nav>
+      <NavLink to="/">EXPLORIX</NavLink>
+      <Bars />
+    </Nav>
+  )
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
+
 
 export default Header
+
+const Nav = styled.nav`
+  background:red;
+  height: 80px;
+  display: flex;
+  justify-content: space-between
+  padding: 0.5rem calc((100vw - 1300px)/2);
+  z-index: 100;
+  position: relative;
+`
+const NavLink = styled(Link)`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height:100%;
+  cursor: pointer;
+`
+
+const Bars = styled(FaBars)`
+  display: none;
+  color: #fff;
+  
+  @media screen and (max-width: 768px) {
+  display: block;
+  position: absolute;
+  top: 0;
+  right:0;
+  transform: translate(-100%, 75%);
+  font-size: 1.8rem;
+  cursor: pointer;
+  }
+`
