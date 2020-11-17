57202e3a33fbf595ef525920667e4413f9683ad4
yasir shahid <yasirshahid5@gmail.com>
2020-11-17 15:34:45 +0500

    update gobalstyle css

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Header from "./Header"
+import {GlobalStyle} from "./styles/GlobalStyles"
 
 
 const Layout = ({ children }) => {
   return (
     <>
+    <GlobalStyle/>
       <Header />
       <main>{children}</main>
 
