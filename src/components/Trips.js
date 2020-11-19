3ed5d6be08bb20c72b38aa331109e738b3539228
yasir shahid <yasirshahid5@gmail.com>
2020-11-19 18:59:20 +0500

    update code 19-11-20

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import styled from 'styled-components';
 
 const Trips = () => {
     return (
-        <div>
-            <h1> Trips</h1>
-        </div>
+        <ProductContainer>
+            <ProductsHeading>Heading</ProductsHeading>
+            <ProductWrapper>Wrapper</ProductWrapper>
+        </ProductContainer>
     )
 }
 
 export default Trips
+
+const ProductContainer = styled.div`
+  min-height: 100vh;
+  padding: 5rem calc((100vw - 1300px) /2);
+  background: red;
+  color: #fff;
+`
+
+const ProductsHeading = styled.div`
+  font-size : clamp(1.2rem, 5vw, 3rem);
+  text-align: center;
+  margin-bottom: 5rem;
+  color: #000;
+`
+
+const ProductWrapper = styled.div``
